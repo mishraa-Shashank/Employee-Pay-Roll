3fba7d64e8d1d8deb705467746a2359028925ef3
Shashank <mishra.shashankofficial06@gmail.com>
2023-01-22 12:34:36 +0530

    Added : Java program to connect to the database

--- src/main/java/DataBaseConnection.java
+++ src/main/java/DataBaseConnection.java
@@ -1,8 +1,28 @@
+import java.sql.Connection;
+import java.sql.DriverManager;
+
 /**
  * @author Shashank
  */
 public class DataBaseConnection {
     public static void main(String[] args) {
         System.out.println("Welcome Employee payroll program");
+        /**
+         * connecting database using java
+         */
+        String jdbcUrl = "jdbc:mysql://localhost:3306/payroll_service";
+        String userName = "root";
+        String password = "Kirti";
+        Connection connection;
+        /*
+        using try catch block to handle the exception
+         */
+        try {
+            System.out.println("Connecting to database: "+jdbcUrl);
+            connection = DriverManager.getConnection(jdbcUrl, userName, password);
+            System.out.println("Connection is successful "+connection);
+        } catch (Exception e) {
+            System.out.println(e);
+        }
     }
 }
