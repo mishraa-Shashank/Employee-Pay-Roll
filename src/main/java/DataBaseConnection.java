d5982766d001c844b2cb0a67996e72e54b716831
Shashank <mishra.shashankofficial06@gmail.com>
2023-01-22 12:29:39 +0530

    Added : Welcome message

---
+++ src/main/java/DataBaseConnection.java
@@ -0,0 +1,8 @@
+/**
+ * @author Shashank
+ */
+public class DataBaseConnection {
+    public static void main(String[] args) {
+        System.out.println("Welcome Employee payroll program");
+    }
+}
